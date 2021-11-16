453ffc1dc00c667a442b16cfddf98447f518253e
Hamza <hamzaelkhatri@gmail.com>
2021-11-16 19:08:07 +0100

    Feat: adding iterators class

--- README.md
+++ README.md
@@ -1,10 +1,35 @@
+static variab
 # ft_containner
 The multiple available containers in C++ all have very different usage. To make sure you understand them, let’s re-implement them!
-
-
 # Resource
 
 http://www.cplusplus.com/reference/vector/vector/
 http://www.cplusplus.com/reference/stack/stack/
 http://www.cplusplus.com/reference/map/map/
 https://www.youtube.com/watch?v=YpNCBw-cPWw
+
+
+BUFFER_SIZE 10
+char *buff mallo
+get_next(int fd)
+ret = read(fd,buff,BUFFER_SIZE)
+{
+
+
+}
+dehdiudehuideh dehudhdeu dhudeudehudeh dheudh \n2
+dehouhdeou
+int inc()
+{
+    static int i = 0;
+    i++;
+    return i;
+}
+
+
+int inc()
+{
+    int i = 0;
+    i++;
+    return i;
+}
---
+++ src/vector.cpp
@@ -0,0 +1,22 @@
+#include <iostream> // std::cout
+#include <iterator> // std::iterator_traits
+#include <typeinfo> // typeid
+
+#include "vector.hpp"
+
+int main()
+{
+    // typedef std::iterator_traits<int *> traits;
+    // if (typeid(traits::iterator_category) == typeid(std::random_access_iterator_tag))
+    //     std::cout << "int* is a random-access iterator\n";
+    // typedef ft::iterators_traits<int *> traits2;
+    // if (typeid(traits2::iterator_category) == typeid(std::random_access_iterator_tag))
+    //     std::cout << "int* is a random-access iterator\n";
+    ft::reverse_iterator<int *> rit(0);
+    // std::cout << "reverse_iterator<int *> rit(0) = " << rit<< '\n';
+    std::reverse_iterator<int *> rit2(0);
+    // rit2.__t
+    // std::cout << "reverse_iterator<int *> rit(0) = " << rit2. << '\n';
+
+    return 0;
+}
